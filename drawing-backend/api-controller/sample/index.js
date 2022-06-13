97bf0706511ad3cb17d234208f346eb59b2f0eb4
skadapure <shakti.kadapure@gmail.com>
2022-06-13 08:18:34 -0400

    prevent server crash

--- drawing-backend/api-controller/sample/index.js
+++ drawing-backend/api-controller/sample/index.js
@@ -1,3 +1,3 @@
 const controller = require("./controller");
 
-module.exports = app => app["get"]("/sample", (req, res, next) => controller(req, res, next));
+module.exports = app => app["get"]("/sample", (req, res, next) => controller(req, res, next).catch(next));
--- drawing-backend/api-controller/upload-image/index.js
+++ drawing-backend/api-controller/upload-image/index.js
@@ -1,3 +1,3 @@
 const controller = require("./controller");
 
-module.exports = app => app["post"]("/images/upload", (req, res, next) => controller(req, res, next));
+module.exports = app => app["post"]("/images/upload", (req, res, next) => controller(req, res, next).catch(next));
